0e58c807385fe72939d57be445117858afcd02be
aazw <3594744+aazw@users.noreply.github.com>
2025-05-06 21:18:05 +0900

    linter実行のスクリプトのコメント修正

--- scripts/go_lint_all.sh
+++ scripts/go_lint_all.sh
@@ -4,5 +4,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/..
 
-# linter (requires bash)
+# linter (bash必須. Bashの機能で*をファイル名に展開)
 golangci-lint run ./playgrounds/*
