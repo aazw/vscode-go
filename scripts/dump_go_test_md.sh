a52fafdeb2413e08cd25813a1281bd45aefef4d1
aazw <3594744+aazw@users.noreply.github.com>
2025-05-06 10:41:29 +0900

    既存スクリプトにコメントでの説明を追加

--- scripts/dump_go_test_md.sh
+++ scripts/dump_go_test_md.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# move to project root
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/..
 
--- scripts/remove_ds_store.sh
+++ scripts/remove_ds_store.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# move to project root
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/..
 
+# delete all .DS_Store
 find . -name ".DS_Store" | xargs rm
