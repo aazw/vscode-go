44e167e94952793d7dd94f9136dd182b794fd9fc
aazw <3594744+aazw@users.noreply.github.com>
2025-05-11 23:31:15 +0900

    標準ライブラリの名前(パス)と概要の出力コマンドなスクリプト追加

---
+++ scripts/go_list_standard_library.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# move to project root
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}/..
+
+# print header
+echo "# Standard library"
+echo ""
+echo "https://pkg.go.dev/std"
+echo ""
+echo "| Package | Since Version | Description |"
+echo "|---------|---------------|-------------|"
+
+# print go std
+go list -json std | jq -r '
+      select(.Standard == true)
+      | select(.ImportPath | test("(^|/)(?:internal|vendor)($|/)") | not)
+      | .ImportPath as $p
+      | (.Doc // "" | split("\n") | .[0]) as $syn
+      | "| https://pkg.go.dev/\($p) | | \($syn) |"
+    ' \
+  | column -t -s '|' -o '|'
