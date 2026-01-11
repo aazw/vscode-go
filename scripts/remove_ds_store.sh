2f1c220fc41c14cfd3cf43589c72a3d39aa31138
aazw <3594744+aazw@users.noreply.github.com>
2026-01-12 06:54:19 +0900

    DevContainer環境を更新

--- scripts/remove_ds_store.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# move to project root
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-cd ${SCRIPT_DIR}/..
-
-# delete all .DS_Store
-find . -name ".DS_Store" -delete
